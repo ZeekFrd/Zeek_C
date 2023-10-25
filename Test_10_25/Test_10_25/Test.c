62e50eac5134b56b3d3d47cbc38afedbbad5e4a6
dphifuture <1877854959@qq.com>
2023-10-25 15:00:13 +0800

    C_杂项

---
+++ Test_10_25/Test_10_25/Test.c
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+//int main()
+//{
+//    union
+//    {
+//        short k;
+//        char i[2];
+//    }*s, a;
+//    s = &a;
+//    s->i[0] = 0x39;
+//    s->i[1] = 0x38;
+//    printf("%x\n", a.k);//十六进制形式输出k
+//    return 0;
+//}
+
+
+
+//
+//enum ENUM_A
+//{
+//	X1,
+//	Y1,
+//	Z1 = 255,
+//	A1,
+//	B1,
+//};
+//int main()
+//{
+//	enum ENUM_A enumA = Y1;
+//	enum ENUM_A enumB = B1;
+//	printf("%d %d\n", enumA, enumB);
+//}
+
+
+
+//int main()
+//{
+//    unsigned char puc[4];
+//    struct tagPIM
+//    {
+//        unsigned char ucPim1;
+//        unsigned char ucData0 : 1;
+//        unsigned char ucData1 : 2;
+//        unsigned char ucData2 : 3;
+//    }*pstPimData;
+//    pstPimData = (struct tagPIM*)puc;
+//    memset(puc, 0, 4);
+//    pstPimData->ucPim1 = 2;
+//    pstPimData->ucData0 = 3;
+//    pstPimData->ucData1 = 4;
+//    pstPimData->ucData2 = 5;
+//    printf("%02x %02x %02x %02x\n", puc[0], puc[1], puc[2], puc[3]);
+//    return 0;
+//}
+
+
+//struct A
+//{
+//    int a;
+//    short b;
+//    int c;
+//    char d;
+//};
+//
+//struct B
+//{
+//    int a;
+//    short b;
+//    char c;
+//    int d;
+//};
+//int main()
+//{
+//    printf("%d %d\n", sizeof(struct A), sizeof(struct B));
+//    return 0;
+//}
